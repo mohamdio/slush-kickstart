559cf75936207b9a4dc4f8db2df134aa9f9a55b2
--replace-all <mandooox@gmail.com>
2016-01-12 15:49:32 +0200

    add sluch-kickstart branch from gulp-workflow

---
+++ templates/gulp/tasks/build/fonts.js
@@ -0,0 +1,22 @@
+// gulp/tasks/build/fonts.js
+'use strict';
+
+// ----------------------------------
+// available tasks: 
+//    'gulp build:fonts' : main fonts task
+// ----------------------------------
+// config:
+//     config.task.build : task name
+// ----------------------------------
+
+module.exports = function(gulp, $, path, config) {
+
+    // copy fonts to prod folder
+    gulp.task(config.task.build + ':fonts', function() {
+
+        return gulp.src(path.to.fonts.dist.dev + '/**/*')
+            .pipe(gulp.dest(path.to.fonts.dist.prod));
+
+    });
+
+};
---
+++ templates/gulp/tasks/build/images.js
@@ -0,0 +1,22 @@
+// gulp/tasks/build/images.js
+'use strict';
+
+// ----------------------------------
+// available tasks: 
+//    'gulp build:images' : main images task
+// ----------------------------------
+// config:
+//     config.task.build : task name
+// ----------------------------------
+
+module.exports = function(gulp, $, path, config) {
+
+    // copy images to prod folder
+    gulp.task(config.task.build + ':images', function() {
+
+        return gulp.src(path.to.images.dist.dev + '/**/*')
+            .pipe(gulp.dest(path.to.images.dist.prod));
+
+    });
+
+};
