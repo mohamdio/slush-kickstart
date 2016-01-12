e7604b23adaccaf2ff9d047741c5764b173c39ef
--replace-all <mandooox@gmail.com>
2016-01-12 16:18:46 +0200

    add sluch-kickstart branch from gulp-workflow

---
+++ templates/gulp/config.js
@@ -0,0 +1,217 @@
+// gulp/config.js
+'use strict';
+
+// require gulp-util & paths
+var gutil    = require('gulp-util'),
+    path     = require('./paths.js'),
+    // require for imagemin options
+    pngquant = require('imagemin-pngquant');
+
+// project local url
+var url = 'gulp.dev';
+
+// define options
+module.exports = {
+
+    // tasks names
+    task: {
+		browserSync: 'serve',
+		fonts      : 'fonts',
+		sass       : 'sass',
+		scripts    : 'js',
+		images     : 'images',
+		bower      : 'bower',
+		nunjucks   : 'nunjucks',
+		clean      : 'clean',
+		build      : 'build'
+    },
+
+    // serve task browser-sync options
+    serve: {
+        dev: {
+            server: {
+            	baseDir: path.to.dist.dev
+            },
+            // proxy: url + '/' + path.to.dev,
+            // set browser automaically opened
+            browser: 'google chrome'
+            // for more options: http://www.browsersync.io/docs/options
+        },
+        prod: {
+        	server: {
+            	baseDir: path.to.dist.prod
+            },
+            // proxy: url + '/' + path.to.prod,
+            // set browser automaically opened
+            browser: 'google chrome'
+        }
+    },
+
+    // bower task mainBowerFiles options
+    mainBowerFiles: {
+        // main options
+        options: {
+            base: 'bower_components'
+        },
+        // bower:css rename options
+        rename: {
+            suffix: "-css",
+            extname: '.scss'
+        },
+        // watch src
+        watch: ['./bower_components/**', './bower.json']
+    },
+
+    // nunjucks task options
+    nunjucks: {
+        // inject task options
+        injectCss: {
+            vendorOptions: {
+                relative: true,
+                name: 'vendor',
+                // removeTags: true
+                // more options : https://github.com/klei/gulp-inject#api
+            },
+            mainOptions: {
+                relative: true,
+                // removeTags: true
+            }
+        },
+        injectJs: {
+            options: {
+                relative: true,
+                // removeTags: true
+            }
+        }
+    },
+
+    // sass task options
+    sass: {
+        // main options
+        options: {
+            includePaths: [
+                path.to.sass.vendor, path.to.sass.foundation, path.to.sass.bootstrap
+            ],
+            outputStyle: 'expanded'
+            // more options
+            // https://github.com/sass/node-sass#usage-1
+        },
+        // sass:doc options
+        sassdocOptions: {
+            dest: path.to.sass.dist.dev + '/_sassdoc',
+            // for more options
+            // http://sassdoc.com/gulp/
+        }
+    },
+
+    // html task options
+    html: {
+        // build:html:copy prettify options
+        prettifyOptions: {
+            indent_size: 2,
+            preserve_newlines: true
+            // for more options: 
+            // https://github.com/beautify-web/js-beautify#css--html
+        },
+        // build:html:inject options
+        injectCss: {
+            options: {
+                relative: true,
+                name: 'build',
+                starttag: '<!-- build:css -->',
+                endtag: '<!-- endbuild -->',
+                removeTags: true
+                // more options : https://github.com/klei/gulp-inject#api
+            }
+        },
+        injectJs: {
+            options: {
+                relative: true,
+                name: 'build',
+                starttag: '<!-- build:js -->',
+                endtag: '<!-- endbuild -->',
+                removeTags: true
+            }
+        }
+    },
+
+    // css task options
+    css: {
+        // uncss options
+        uncssOptions: {
+            html: [path.to.dist.dev + '*.html'],
+            // more options
+            // https://github.com/giakki/uncss#within-nodejs
+        },
+        // strip-css-comments options
+        stripCommentsOptions: {
+            preserve: true
+        },
+        // cssbeautify options
+        cssbeautifyOptions: {
+            indent: '  '
+        },
+        // minify css options
+        minifyCssOptions: {
+            keepSpecialComments: 1
+        },
+        // rename options
+        renameOptions: {
+            suffix: '.min'
+        }
+    },
+
+    // images task options
+    images: {
+        // imagemin options
+        imageminOptions: {
+            optimizationLevel: 7,
+            progressive: true,
+            interlaced: true,
+            use: [pngquant()] // imagemin-pngquant
+        },
+        // favicons options
+        faviconsOptions: {
+            appName: 'Gulpfile Setup',
+            appDescription: 'Gulpfile setup',
+            developerName: 'mohamdio',
+            developerURL: 'http://mohamd.io/',
+            background: '#020307',
+            path: 'assets/images/favicons/',
+            url: '/',
+            display: 'standalone',
+            orientation: 'portrait',
+            version: 1.0,
+            logging: false,
+            online: false,
+            html: path.to.nunjucks.config + 'includes/favicons.nunjucks',
+            replace: true,
+            icons: {
+                android: false, // create Android homescreen icon
+                appleIcon: false, // create Apple touch icons
+                appleStartup: false, // create Apple startup images
+                coast: false, // create Opera Coast icon
+                favicons: true, // create regular favicons
+                firefox: false, // create Firefox OS icons
+                opengraph: false, // create Facebook OpenGraph image
+                twitter: false, // create Twitter Summary Card image
+                windows: false, // create Windows 8 tile icons
+                yandex: false // create Yandex browser icon
+            }
+            // options from here : https://github.com/haydenbleasel/favicons#nodejs
+        }
+    },
+
+    // error handler
+    error: function(error) {
+        // output an error message
+        gutil.log(
+            gutil.colors.red(
+                '\n\------------------------------\n\Error in plugin (' + gutil.colors.green(error.plugin) + '):\n\ ' + gutil.colors.blue(error.message) + '------------------------------'
+            )
+        );
+        // emit the end event, to properly end the task
+        this.emit('end');
+    }
+    
+};
---
+++ templates/gulp/tasks/default/scripts.js
@@ -0,0 +1,80 @@
+// gulp/tasks/default/scripts.js
+'use strict';
+
+// ----------------------------------
+// available tasks: 
+//    'gulp js'            : main js task
+//    'gulp js:browserify' : browserify task
+//    'gulp js:copySrc'    : copy source js files
+// ----------------------------------
+// plugins:
+//     browserify         : $.browserify
+//     vinyl-source-stream: $.vinylSourceStream
+//     vinyl-buffer       : $.vinylBuffer
+//     watchify           : $.watchify
+//     browser-sync       : $.browserSync
+//     gulp-sourcemaps    : $.sourcemaps
+//     lodash.assign      : $.assign
+//     gulp-uglify        : $.uglify
+// ----------------------------------
+// config:
+//     config.task.scripts : task name
+// ----------------------------------
+
+module.exports = function(gulp, $, path, config) {
+
+    // custom browserify options
+    var customOpts = {
+        entries: path.to.js.src.main,
+        debug: true
+    };
+    var opts = $.assign({}, $.watchify.args, customOpts);
+    var b = $.watchify($.browserify(opts));
+
+    // browserify task
+    gulp.task(config.task.scripts + ':browserify', bundle);
+
+    // watchify update
+    b.on('update', bundle);
+
+    // output build logs to terminal
+    b.on('log', $.util.log);
+
+    // browserify function
+    function bundle() {
+        return b.bundle()
+            .on('error', config.error)
+            .pipe($.vinylSourceStream('scripts.js'))
+            .pipe($.vinylBuffer())
+            .pipe($.sourcemaps.init({
+                loadMaps: true
+            }))
+            .pipe($.uglify())
+            .on('error', config.error)
+            .pipe($.sourcemaps.write('.'))
+            .pipe(gulp.dest(path.to.js.dist.dev))
+            .pipe($.browserSync.reload({
+                stream: true
+            }));
+    }
+
+    // copy source js files to build/dev
+    gulp.task(config.task.scripts + ':copySrc', function() {
+
+        return gulp.src(path.to.js.src.copy)
+            .pipe(gulp.dest(path.to.js.dist.dev + '/src'));
+
+    });
+
+    // main js task
+    gulp.task(config.task.scripts, function(cb) {
+
+        $.runSequence(
+            config.task.scripts + ':browserify',
+            config.task.scripts + ':copySrc',
+            cb
+        )
+
+    });
+
+};
